269bd98ef7861cc45f109580cbd141fa8c767bf4
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-03 21:32:57 +1200

    printing out the welcome player meesage

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -1,13 +1,18 @@
 package nz.ac.auckland.se281;
 
+
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  int i = 0;
+  String humanName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    // print welcome meesage
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
