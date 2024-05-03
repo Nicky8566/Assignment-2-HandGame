cc8e1f250712b929dd2227185222a4a38edfff00
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-03 22:06:44 +1200

    printed out hand information meesage with correct name and fingers. Also added variables for the player name and round and number of fingers for the play method

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,15 +5,20 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  int round = 0;
+  String playerName;
+  String numberFingers;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    playerName = options[0];
   }
 
   public void play() {
+    round++;
     // print start round meesage
-    MessageCli.START_ROUND.printMessage("1");
+    MessageCli.START_ROUND.printMessage(String.valueOf(round));
     // print ask input meesage
     MessageCli.ASK_INPUT.printMessage();
     // go through a while loop until the input is correct
@@ -22,12 +27,15 @@ public class Game {
       if (Utils.isInteger(input)) {
         int fingers = Integer.parseInt(input);
         if (fingers >= 0 && fingers <= 5) {
+          numberFingers = input;
           break;
         }
       }
       // print error meesage
       MessageCli.INVALID_INPUT.printMessage();
     }
+    // print info hand meesage
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, numberFingers);
   }
 
   public void endGame() {}
