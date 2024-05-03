f28a693d0c815044258d4ebee8167adafdd9707d
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-04 00:54:02 +1200

    changed the difficulty file names to captial letters because of code style convention. Also added if statments for the diffculty of the robot, logic in the if statements yet to be coded

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -2,17 +2,20 @@ package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
+import nz.ac.auckland.se281.difficulty.difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
   private int round = 0;
   private String playerName;
   private String numberFingers;
+  private Difficulty howDiffcult;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
     playerName = options[0];
+    howDiffcult = difficulty;
   }
 
   public void play() {
@@ -37,6 +40,13 @@ public class Game {
     // print info hand meesage
     MessageCli.PRINT_INFO_HAND.printMessage(
         String.format("Player %s: fingers: %s", playerName, numberFingers));
+    // do if statements for the bots difficulty
+    if (howDiffcult == Difficulty.EASY) {
+    } else if (howDiffcult == Difficulty.MEDIUM) {
+      // do medium bot
+    } else if (howDiffcult == Difficulty.HARD) {
+      // do hard bot
+    }
   }
 
   public void endGame() {}
