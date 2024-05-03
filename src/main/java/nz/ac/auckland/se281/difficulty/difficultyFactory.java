a8b70002b4967ce48b159c2d705b9a293fab747a
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-04 00:45:33 +1200

    set up the difficulty factory file as how i think it should work

--- src/main/java/nz/ac/auckland/se281/difficulty/difficultyFactory.java
+++ src/main/java/nz/ac/auckland/se281/difficulty/difficultyFactory.java
@@ -1,3 +1,21 @@
 package nz.ac.auckland.se281.difficulty;
 
-public class difficultyFactory {}
+public class difficultyFactory {
+  public static difficulty chooseDifficulty(String difficulty) {
+    switch (difficulty) {
+      case "E":
+        return new easy();
+
+      case "M":
+        return new medium();
+
+      case "H":
+        return new hard();
+
+      default:
+        System.err.println("Something went wrong!");
+        System.exit(0);
+    }
+    return null;
+  }
+}
