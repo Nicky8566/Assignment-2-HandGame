dd70e84fbf1c52c7e6585f78e6572cfba54267e5
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-03 21:45:28 +1200

    made a while loop for the amount of fingers. It will continue printing the error meesage until a correct input

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -9,8 +9,6 @@ public class Game {
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-    // print welcome meesage
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {
@@ -18,6 +16,18 @@ public class Game {
     MessageCli.START_ROUND.printMessage("1");
     // print ask input meesage
     MessageCli.ASK_INPUT.printMessage();
+    // go through a while loop until the input is correct
+    while (true) {
+      String input = Utils.scanner.nextLine();
+      if (Utils.isInteger(input)) {
+        int fingers = Integer.parseInt(input);
+        if (fingers >= 0 && fingers <= 5) {
+          break;
+        }
+      }
+      // print error meesage
+      MessageCli.INVALID_INPUT.printMessage();
+    }
   }
 
   public void endGame() {}
