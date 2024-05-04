ee25f7a047196c37f46729b4377a7dab243aa6f9
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-05-04 14:57:15 +1200

    set up the strategy changer file

--- src/main/java/nz/ac/auckland/se281/strategy/StrategyChanger.java
+++ src/main/java/nz/ac/auckland/se281/strategy/StrategyChanger.java
@@ -1,3 +1,17 @@
 package nz.ac.auckland.se281.strategy;
 
-public class StrategyChanger {}
+public class StrategyChanger {
+  private Strategy strategy;
+
+  public StrategyChanger(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void setStrategy(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void process() {
+    strategy.execute();
+  }
+}
